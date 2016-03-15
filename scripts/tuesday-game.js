260f955f76912cac35495030f5ff085e7a24d051
Jeremy Brown <jj.brown@hotmail.com>
2016-03-15 07:33:05 -0400

    Progress

--- scripts/tuesday-game.js
+++ scripts/tuesday-game.js
@@ -1,1 +1,17 @@
-var title = TweenLite.to("#title", 3, {y:300});
+
+function startGame() {
+  TweenLite.to("#start-button", 2, {opacity:0});
+
+  // Remove start button
+  var start_btn = document.getElementById('image-section').innerHTML;
+  start_btn.parentNode.removeChild(start_btn);
+
+  // Set title (TUESDAY) as main element
+  var TUESDAY = document.createElement("H1");
+  var t = document.createTextNode("TUESDAY");
+  // TUESDAY.setAttribute() id, opacity
+  TUESDAY.appendChild(t);
+  document.body.appendChild(t);
+
+  TweenLite.to("#tuesday-title", 2, {opacity:1});
+}
