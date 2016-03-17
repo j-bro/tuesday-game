859c85117b8acf9d4865607ee72f9206e0f859e5
Jeremy Brown <jj.brown@hotmail.com>
2016-03-16 23:29:11 -0400

    More progess on round 1 + refactoring

--- scripts/scenes.js
+++ scripts/scenes.js
@@ -1,4 +1,102 @@
 
-function tuesdayTitle() {
-  
+function tuesdayTitleScene() {
+  var t = new TimelineLite();
+  t.call(function() {
+    var tuesday_title = document.createElement("H1");
+    tuesday_title.innerHTML = "TUESDAY";
+    tuesday_title.setAttribute("id", "tuesday-title");
+    addImage(tuesday_title);
+  });
+  t.delay(1);
+  t.to("#image-section", 3, {opacity:1});
+  t.to("#image-section", 3, {opacity:0}, "+=2");
+
+  t.call(function() {
+    removeImage();
+    var tuesday_subtitle = document.createElement("H3");
+    tuesday_subtitle.innerHTML = "by Julie Brown";
+    tuesday_subtitle.setAttribute("id", "tuesday-subtitle");
+    addImage(tuesday_subtitle);
+  });
+  t.to("#image-section", 3, {opacity:1});
+  t.to("#image-section", 3, {opacity:0}, "+=2");
+  t.call(function() {
+    removeImage();
+    r1Scene();
+  });
+}
+
+function r1Scene() {
+  var t = new TimelineLite();
+  t.call(function() {
+    var r1Title = document.createElement("H1");
+    r1Title.innerHTML = "[ROUND ONE]";
+    r1Title.setAttribute("id", "r1-title");
+    addImage(r1Title);
+  });
+  t.to("#image-section", 3, {opacity:1});
+  t.to("#image-section", 3, {opacity:0}, "+=2");
+  t.call(function() {
+    removeImage();
+    r1WindowScene();
+  });
+}
+
+function r1WindowScene() {
+  var t = new TimelineLite();
+  t.call(function() {
+    addImage({"alt": "",
+              "src": "img/gifs/window_gif.gif",
+              "id": "r1-window-image"});
+  });
+  t.to("#image-section", 3, {opacity:1});
+  t.call(function() {
+    addText("It is Tuesday. It is raining.");
+    addText("// This always happens.");
+  });
+  t.to("#text-section", 2, {opacity:1}, "+=2");
+  t.to("#text-section", 2, {opacity:0}, "+=2");
+  t.call(function() {
+    removeText();
+    addText("What will you do?");
+    addText("> Wait.", {'onclick': 'r1StopRainScene()'});
+    addText("> Watch.", {'onclick': 'r1StopRainScene()'});
+  });
+  t.to("#text-section", 2, {opacity:1}, "+=3");
+}
+
+function r1StopRainScene() {
+  var t = new TimelineLite();
+  t.to("#text-section", 2, {opacity:0});
+  // TODO Slow and stop rain
+  t.to("#r1-window-image", 1, {src: "img/gifs/window_slow2.gif"}, "+=2");
+  t.to("#r1-window-image", 3, {src: "img/frames/window_norain_bad.png"}, "+=2");
+  // TODO fade window
+  // TODO show door
+  t.call(function() {
+    removeText();
+    addText("It is Tuesday.");
+    addText("// Do you know what will happen?");
+    addText("> Yes", {"onclick": "r1AfterRainYesScene()"});
+    addText("> No", {"onclick": "r1AfterRainNoScene()"});
+  });
+  t.to("#text-section", 2, {opacity:1});
+}
+
+function r1AfterRainYesScene() {
+  var t = new TimelineLite();
+  t.to("#text-section", 2, {opacity:0});
+  t.call(function() {
+    removeText();
+    addText("So you say.");
+  });
+}
+
+function r1AfterRainNoScene() {
+  var t = new TimelineLite();
+  t.to("#text-section", 2, {opacity:0});
+  t.call(function() {
+    removeText();
+    addText("Watch closely.");
+  });
 }
--- scripts/tuesday-game.js
+++ scripts/tuesday-game.js
@@ -16,99 +16,51 @@ function startGame() {
   // TweenLite.to("#tuesday-title", 2, {opacity:1});
 }
 
-function addImage(image_element) {
-  var image_container = document.createElement("DIV");
-  image_container.setAttribute("id", "image-container");
-  image_container.appendChild(image_element);
+function addImage(attributes) {
+  var imageElement = document.createElement("IMG");
+  attributes = attributes || {};
+  for (var key in attributes) {
+    if (attributes.hasOwnProperty(key)) {
+      imageElement.setAttribute(key, attributes[key]);
+    }
+  }
 
-  var image_section = document.getElementById("image-section");
-  image_section.appendChild(image_container);
+  var imageContainer = document.createElement("DIV");
+  imageContainer.setAttribute("id", "image-container");
+  imageContainer.appendChild(imageElement);
 
-  return image_container;
+  var imageSection = document.getElementById("image-section");
+  imageSection.appendChild(imageContainer);
+
+  return imageContainer;
 }
 
-function addText(text) {
-  var text_element = document.createElement("P");
-  text_element.innerHTML = text;
+function addText(text, attributes) {
+  var textElement = document.createElement("P");
+  textElement.innerHTML = text;
+  attributes = attributes || {};
+  for (var key in attributes) {
+    if (attributes.hasOwnProperty(key)) {
+      textElement.setAttribute(key, attributes[key]);
+    }
+  }
 
-  var text_container = document.createElement("DIV");
-  text_container.setAttribute("id", "text-container");
-  text_container.appendChild(text_element);
+  var textContainer = document.createElement("DIV");
+  textContainer.setAttribute("id", "text-container");
+  textContainer.appendChild(textElement);
 
-  var text_section = document.getElementById("text-section");
-  text_section.appendChild(text_container);
+  var textSection = document.getElementById("text-section");
+  textSection.appendChild(textContainer);
 
-  return text_container;
+  return textSection;
 }
 
 function removeImage() {
-  var image_container = document.getElementById("image-container");
-  image_container.parentNode.removeChild(image_container);
+  var imageContainer = document.getElementById("image-section");
+  imageContainer.innerHTML = "";
 }
 
 function removeText() {
-  var text_container = document.getElementById("text-container");
-  // TweenLite.to(text_container, 2, {opacity:0});
-  text_container.parentNode.removeChild(text_container);
-}
-
-
-
-function tuesdayTitleScene() {
-  var t = new TimelineLite();
-  t.call(function() {
-    var tuesday_title = document.createElement("H1");
-    tuesday_title.innerHTML = "TUESDAY";
-    tuesday_title.setAttribute("id", "tuesday-title");
-    addImage(tuesday_title);
-  });
-  t.delay(1);
-  t.to("#image-section", 3, {opacity:1});
-  t.to("#image-section", 3, {opacity:0}, "+=2");
-
-  t.call(function() {
-    removeImage();
-    var tuesday_subtitle = document.createElement("H3");
-    tuesday_subtitle.innerHTML = "by Julie Brown";
-    tuesday_subtitle.setAttribute("id", "tuesday-subtitle");
-    addImage(tuesday_subtitle);
-  });
-  t.to("#image-section", 3, {opacity:1});
-  t.to("#image-section", 3, {opacity:0}, "+=2");
-  t.call(function() {
-    removeImage();
-    r1Scene();
-  });
-}
-
-function r1Scene() {
-  var t = new TimelineLite();
-  t.call(function() {
-    var r1Title = document.createElement("H1");
-    r1Title.innerHTML = "[ROUND ONE]";
-    r1Title.setAttribute("id", "r1-title");
-    addImage(r1Title);
-  });
-  t.to("#image-section", 3, {opacity:1});
-  t.to("#image-section", 3, {opacity:0}, "+=2");
-  t.call(function() {
-    removeImage();
-    r1WindowScene();
-  });
-}
-
-function r1WindowScene() {
-  var t = new TimelineLite();
-  t.call(function() {
-    var r1WindowImage = document.createElement("IMG");
-    r1WindowImage.setAttribute("alt", "");
-    r1WindowImage.setAttribute("src", "img/gifs/window_gif.gif");
-    r1WindowImage.setAttribute("id", "r1-window-image");
-    addImage(r1WindowImage);
-  });
-  t.to("#image-section", 3, {opacity:1});
-  t.call(function() {
-    addText("It is Tuesday. It is raining.  // This always happens.");
-  });
-  t.to("#text-section", 2, {opacity:1}, "+-3");
+  var textContainer = document.getElementById("text-section");
+  textContainer.innerHTML = "";
 }
